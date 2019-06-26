7caf98349e84e91080b1ce0d7d7aa2e58706821e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-26 14:31:14 +0700

    :newspaper: add notification fragment

---
+++ app/src/main/java/com/yoesuv/navigationadvanced/NotificationFragment.kt
@@ -0,0 +1,15 @@
+package com.yoesuv.navigationadvanced
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NotificationFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+}

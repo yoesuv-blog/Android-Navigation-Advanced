fb04defa05de9a7e4b42d2be4ed789d1ff3baa77
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-26 16:45:29 +0700

    :art: disable shift mode bottom navigation

--- app/src/main/java/com/yoesuv/navigationadvanced/MainActivity.kt
+++ app/src/main/java/com/yoesuv/navigationadvanced/MainActivity.kt
@@ -2,11 +2,17 @@ package com.yoesuv.navigationadvanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.navigationadvanced.utils.BottomNavigationViewHelper
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbarMain)
+        bottomNavigation.itemIconTintList = null
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigation)
     }
 }

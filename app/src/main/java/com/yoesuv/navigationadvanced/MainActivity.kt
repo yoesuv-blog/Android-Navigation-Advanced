d4be37e27fef5037fd376dd1d135c560faa0cfa1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-27 09:27:35 +0700

    :pencil: prepare navigation

--- app/src/main/java/com/yoesuv/navigationadvanced/MainActivity.kt
+++ app/src/main/java/com/yoesuv/navigationadvanced/MainActivity.kt
@@ -2,6 +2,9 @@ package com.yoesuv.navigationadvanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.yoesuv.navigationadvanced.utils.BottomNavigationViewHelper
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,5 +17,30 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbarMain)
         bottomNavigation.itemIconTintList = null
         BottomNavigationViewHelper.disableShiftMode(bottomNavigation)
+
+        setupNavigation()
+        setupBottomNavigation()
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.fragmentMain).navigateUp()
+    }
+
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.fragmentMain)
+        setupActionBarWithNavController( navController)
+        NavigationUI.setupWithNavController(toolbarMain, navController)
+    }
+
+    private fun setupBottomNavigation() {
+        bottomNavigation.setOnNavigationItemSelectedListener { menu ->
+            when{
+                menu.itemId == R.id.menuHome -> {  }
+                menu.itemId == R.id.menuDashboard -> { }
+                menu.itemId == R.id.menuNotification -> {  }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
 }

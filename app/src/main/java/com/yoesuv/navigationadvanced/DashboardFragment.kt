b2d7bb8ef7889ecc8663bc8cfdc211e7c25e30bd
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-28 09:29:34 +0700

    :sparkles: dashboard detail action

--- app/src/main/java/com/yoesuv/navigationadvanced/DashboardFragment.kt
+++ app/src/main/java/com/yoesuv/navigationadvanced/DashboardFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.buttonDashboard.setOnClickListener{
+            it.findNavController().navigate(R.id.action_dashboard_to_dashboard_detail)
+        }
     }
 }

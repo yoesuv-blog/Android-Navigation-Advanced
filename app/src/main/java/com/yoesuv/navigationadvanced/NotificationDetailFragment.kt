8e03baed943da76cecc93352a8e695ee4a4b03d1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-07-01 10:58:39 +0700

    :sparkles: send data via arguments

--- app/src/main/java/com/yoesuv/navigationadvanced/NotificationDetailFragment.kt
+++ app/src/main/java/com/yoesuv/navigationadvanced/NotificationDetailFragment.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_notification_detail.view.*
 
 class NotificationDetailFragment: Fragment() {
 
+    private val args: NotificationDetailFragmentArgs by navArgs()
+    private var sampleDataModel: SampleDataModel? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        sampleDataModel = args.dataDetailNotification
         return inflater.inflate(R.layout.fragment_notification_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.textViewNotificationDetail.text = sampleDataModel?.value
     }
 
 }

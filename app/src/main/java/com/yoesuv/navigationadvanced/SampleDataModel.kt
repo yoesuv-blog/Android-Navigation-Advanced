3ef2cc7c453de09c67da5d568ee843c47913e2df
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-07-01 09:35:50 +0700

    :pencil: create data model class

---
+++ app/src/main/java/com/yoesuv/navigationadvanced/SampleDataModel.kt
@@ -0,0 +1,7 @@
+package com.yoesuv.navigationadvanced
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SampleDataModel (val value: String?): Parcelable
